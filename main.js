df1516486e6a519dd5b0a7b36f2b548a634dbb40
CodeSekara <92124764+CodeSekara@users.noreply.github.com>
2021-10-08 03:11:32 +0530

    Now you can download

---
+++ main.js
@@ -0,0 +1,23 @@
+function locate() {
+    var searchtarget= document.getElementById("userinput").value; 
+    window.open("https://www.youtube.com/results?search_query="+searchtarget+"&sp=EgIwAQ%253D%253D",'_blank');
+}
+function locatetwo() {
+    var searchtarget= document.getElementById("userinput").value; 
+    window.open("https://www.youtube.com/results?search_query="+searchtarget+"&sp=EgQIBTAB",'_blank');
+}
+function locatethree() {
+    var searchtarget= document.getElementById("userinput").value; 
+    window.open("https://www.youtube.com/results?search_query="+searchtarget+"&sp=EgYIBBABMAE%253D",'_blank');
+}
+function locatefour() {
+    var searchtarget= document.getElementById("userinput").value; 
+    window.open("https://www.youtube.com/results?search_query="+searchtarget+"&sp=EgYIAxABMAE%253D",'_blank');
+}
+/*
+>> Written by:GOW--Subodha Wijesekara.
+>> Compatible with any Web browser.
+>> You can edit every numeric values and colors in this JAVASCRIPT code.
+>> Be strong with your unique skill.
+>> See you in highest place that your dream ends.
+*/
